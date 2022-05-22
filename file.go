bca0a1e166720ac5910567e0d20e42a44048ee49
Olusesi Adebisi <olusesa@wyztechsolutions.com>
2022-05-22 09:39:13 +0100

    updated

--- file.go
+++ file.go
@@ -1,7 +1,5 @@
 package main
 import "fmt"
-//import pb "github.com/olusesa/wyzlab-proto-golan/proto"
-import "google.golang.org/protobuf/proto"
 
 func writeToFile(fname string, pb proto.Message){
 	out, err := proto.Marshal(pb);
